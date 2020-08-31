6dbb06ff228fd52129036075ac9ed46cddbb8689
cihankarluk <cihan.karluk@akinon.com>
2020-08-31 23:28:29 +0300

    Add CreatePlaylistView test cases

--- tests/music/test_views.py
+++ tests/music/test_views.py
@@ -7,7 +7,7 @@ from model_mommy import mommy
 from rest_framework.test import APIClient
 
 from musicwire.account.models import UserProfile
-from musicwire.music.models import Playlist, PlaylistTrack
+from musicwire.music.models import Playlist, PlaylistTrack, CreatedPlaylist
 
 
 class PlaylistViewTestCase(TestCase):
@@ -349,3 +349,171 @@ class PlaylistTrackViewTestCase(TestCase):
             )
 
         self.assertEqual(expected_result, response.json())
+
+
+class CreatePlaylistViewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.url = reverse("create_playlist")
+        self.user1 = mommy.make(UserProfile, token='user1_token')
+        self.user2 = mommy.make(UserProfile, token='user2_token')
+        self.headers = {
+            "Content-Type": "application/json",
+            "HTTP_TOKEN": "user1_token"
+        }
+
+    def test_get_created_playlist_if_user_is_not_authenticated(self):
+        expected_result = {
+            'status_code': 401,
+            'code': 'AUTHENTICATION_FAIL',
+            'error_message': 'Authentication Failed'
+        }
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(expected_result, response.json())
+
+    def test_get_created_playlist_if_user_is_authenticated(self):
+        mommy.make(CreatedPlaylist, user=self.user1)
+        mommy.make(CreatedPlaylist, user=self.user2)
+
+        response = self.client.get(
+            self.url,
+            **self.headers
+        )
+
+        self.assertEqual(1, response.json()['count'])
+
+    def test_get_created_playlist_if_filter_playlists(self):
+        mommy.make(
+            CreatedPlaylist,
+            user=self.user1,
+            name="test_name_1",
+            status="test_status",
+            remote_id="test_remote_id_1",
+            provider="test_provider",
+        )
+        mommy.make(
+            CreatedPlaylist,
+            user=self.user1,
+            name="test_name_2",
+            status="test_status",
+            remote_id="test_remote_id_2",
+            provider="test_provider",
+        )
+
+        request_data = {
+            "name": "test_name_1",
+            "status": "test_status",
+            "playlist_id": "test_remote_id_1",
+            "provider": "test_provider"
+        }
+
+        response = self.client.get(
+            self.url,
+            **self.headers,
+            data=request_data
+        )
+
+        expected_result = {
+            'count': 1,
+            'next': None,
+            'previous': None,
+            'results': [{'name': 'test_name_1',
+                         'status': 'test_status',
+                         'remote_id': 'test_remote_id_1',
+                         'provider': 'test_provider'}]
+        }
+
+        self.assertEqual(expected_result, response.json())
+
+    def test_post_create_playlist_if_user_is_not_authenticated(self):
+        expected_result = {
+            'status_code': 401,
+            'code': 'AUTHENTICATION_FAIL',
+            'error_message': 'Authentication Failed'
+        }
+
+        response = self.client.post(self.url)
+
+        self.assertEqual(expected_result, response.json())
+
+    def test_post_create_playlist_if_extra_parameter_in_request_data(self):
+        request_data = {
+            "end": "youtube",
+            "end_token": "test_token",
+            "playlist_name": "test_playlist_name",
+            "privacy_status": "test_privacy_status",
+            "collaborative": "test_collaborative",
+            "description": "test_description",
+            "test_parameter": "test_parameter"
+        }
+
+        expected_result = {
+            'status_code': 400,
+            'code': 'VALIDATION_ERROR',
+            'error_message': {
+                'non_field_errors': ["Got unknown fields: {'test_parameter'}"]
+            }
+        }
+
+        response = self.client.post(
+            self.url,
+            data=json.dumps(request_data),
+            content_type="application/json",
+            **self.headers
+        )
+
+        self.assertEqual(expected_result, response.json())
+
+    def test_post_create_playlist_if_end_is_spotify(self):
+        """
+        If end is spotify user id is required.
+        """
+        request_data = {
+            "end": "spotify",
+            "end_token": "test_token",
+            "playlist_name": "test_playlist_name",
+            "privacy_status": "test_privacy_status",
+            "collaborative": "test_collaborative",
+            "description": "test_description",
+        }
+
+        expected_result = {
+            'status_code': 400,
+            'code': 'VALIDATION_ERROR',
+            'error_message': {'end': ['user_id is required for Spotify.']}
+        }
+
+        response = self.client.post(
+            self.url,
+            data=json.dumps(request_data),
+            content_type="application/json",
+            **self.headers
+        )
+
+        self.assertEqual(expected_result, response.json())
+
+    def test_post_create_playlist_if_user_is_authenticated(self):
+        request_data = {
+            "end": "spotify",
+            "end_token": "test_token",
+            "playlist_name": "test_playlist_name",
+            "privacy_status": "test_privacy_status",
+            "collaborative": "test_collaborative",
+            "description": "test_description",
+            "user_id": "test_user_id"
+        }
+
+        patch_address = "musicwire.provider.adapters.spotify.Adapter.create_playlist"
+        return_value = []
+
+        with patch(patch_address, return_value=return_value) as p:
+            self.client.post(
+                self.url,
+                data=json.dumps(request_data),
+                content_type="application/json",
+                **self.headers
+            )
+
+        p.assert_called_with(request_data)
